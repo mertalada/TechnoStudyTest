2d273d41f56f01c0654ce4cc192ed3af6213b922
Mert Aladağ <heavyalad@gmail.com>
2023-09-07 16:46:49 +0300

    düzeltmeler

--- src/TechnoStudy.java
+++ src/TechnoStudy.java
@@ -12,45 +12,5 @@ public class TechnoStudy {
     public void TechnoStudyTest() {
         driver.get("https://techno.study");
 
-        MyFunction.Wait(1);
-
-        WebElement sdet = driver.findElement(By.xpath("//*[@id='rec482366375']/div/div/div[2]/div[2]/ul/li[1]/a"));
-        sdet.click();
-
-    //    WebElement leanmoreSDET = driver.findElement(By.xpath("//a[@href='#rec491903214'"));
-    //    leanmoreSDET.click();
-
-        MyFunction.Wait(1);
-
-       WebElement android = driver.findElement(By.className("t-menu__link-item t966__tm-link"));
-       android.click();
-
-       WebElement a=driver.findElement(By.className("t966__menu-item-title t-name"));
-       a.click();
-
-    //   MyFunction.Wait(1);
-
-    //   WebElement leanmoreANDROID = driver.findElement(By.className("tn-atom"));
-    //   leanmoreANDROID.click();
-
-       MyFunction.Wait(1);
-
-       WebElement datasience = driver.findElement(By.xpath("//*[@class='t396']/div/div/div/div/div/div/a"));
-       datasience.click();
-
-    //   MyFunction.Wait(1);
-
-    //   WebElement leanmoreDATA = driver.findElement(By.className("tn-atom"));
-    //   leanmoreDATA.click();
-
-       MyFunction.Wait(1);
-
-       WebElement master = driver.findElement(By.xpath("//*[@id=\"rec491928654\"]/div/div/div[2]/div[2]/ul/li[4]/a"));
-       master.click();
-
-    //   MyFunction.Wait(1);
-
-    //   WebElement leanmoreMASTER = driver.findElement(By.className("tn-atom"));
-    //   leanmoreMASTER.click();
     }
 }
