77d64e9ff34aa8764cd073d7ac07071baf323b53
cnkeskin <cankeskinx@gmail.com>
2023-09-08 17:05:27 +0300

    Can Keskin Basvuru Test Hazır

--- src/TechnoStudy.java
+++ src/TechnoStudy.java
@@ -15,5 +15,43 @@ public class TechnoStudy {
         leadcodes.programs();
         leadcodes.kurslar();
 
+
+
+    }
+    @Test
+    public void BasvuruTest() {
+        driver.get("https://techno.study/tr");
+
+        MyFunction.Wait(1);
+
+        WebElement basvur=driver.findElement(By.xpath("//a[@class='tn-atom js-click-zero-stat']"));
+        basvur.click();
+
+
+        WebElement adsoyad=driver.findElement(By.xpath("//input[@placeholder='Adı Soyadı']"));
+        adsoyad.sendKeys("test test");
+
+
+        WebElement email=driver.findElement(By.name("email"));
+        email.sendKeys("test_sdet0106@gmail.com");
+
+        WebElement telephone=driver.findElement(By.xpath("//input[@type='tel']"));
+        telephone.sendKeys("9999999999");
+
+        WebElement ulke=driver.findElement(By.id("sb-1667664755026"));
+        ulke.sendKeys("Andorra");
+
+        WebElement kurs=driver.findElement(By.name("course"));
+        kurs.sendKeys("SDET Türkçe");
+
+        WebElement nerdenduydun=driver.findElement(By.name("survey"));
+        nerdenduydun.sendKeys("Başka");
+
+        WebElement kutu=driver.findElement(By.cssSelector("[class='t-checkbox__indicator']"));
+        kutu.click();
+
+        WebElement gonder=driver.findElement(By.xpath("//button[@type='submit']"));
+        gonder.click();
+
     }
 }
